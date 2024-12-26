bb881e5c40314079e0fd920db7f6c88ff9bf5b4a
pratiksagare <79927151+pratiksagare@users.noreply.github.com>
2024-12-26 17:44:12 +0530

    adding text

--- src/components/ChatsContainer.jsx
+++ src/components/ChatsContainer.jsx
@@ -1,19 +1,9 @@
-// export default function ChatsContainer() {
-//     return (
-//         <div className="grid grid-cols-3">
-//             <div className="w-[33%] h-auto bg-red-500 aspect-square"></div>
-//             <div className="w-[33%] h-auto bg-green-500 aspect-square"></div>
-//             <div className="w-[33%] h-auto bg-blue-500 aspect-square"></div>
-//         </div>
-//     );
-// }
 export default function ChatsContainer() {
-    return (
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        <div className="bg-red-500 aspect-[4/8]"></div>
-        <div className="bg-green-500 aspect-[4/8]"></div>
-        <div className="bg-blue-500 aspect-[4/8]"></div>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 text-white">
+      <div className="bg-red-500 aspect-[1/2] flex justify-center items-center">ChatGPT</div>
+      <div className="bg-green-500 aspect-[1/2] flex justify-center items-center">Gemini</div>
+      <div className="bg-blue-500 aspect-[1/2] flex justify-center items-center">GPT</div>
+    </div>
+  );
+}
